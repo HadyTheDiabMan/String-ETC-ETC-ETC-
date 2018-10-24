985e69ff136202fe048d34fad15283e6ffe347c9
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2018-10-24 12:44:18 -0400

    Add files via upload

---
+++ src/StringManipulator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Hady Diab, String Manipulator
+ *
+ *
+ */
+public class StringManipulator 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner Yeet=new Scanner(System.in);
+		System.out.print("Hi User, enter in any string:");
+		
+
+	
+	}
+
+
+}
